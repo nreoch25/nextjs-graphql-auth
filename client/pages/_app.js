81990db6b8cbbae9c9d538fbc1c231a2b671792f
Nigel Reoch <nreoch9@gmail.com>
2018-12-19 17:57:57 -0500

    Setup Apollo on the client with Nextjs

---
+++ client/pages/_app.js
@@ -0,0 +1,18 @@
+import App, { Container } from "next/app";
+import { ApolloProvider } from "react-apollo";
+import withApollo from "../lib/withApollo";
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps, apollo } = this.props;
+    return (
+      <Container>
+        <ApolloProvider client={apollo}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </Container>
+    );
+  }
+}
+
+export default withApollo(MyApp);
---
+++ client/pages/test.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Test extends Component {
+  render() {
+    return (
+      <div>
+        <p>Test</p>
+      </div>
+    );
+  }
+}
+
+export default Test;
