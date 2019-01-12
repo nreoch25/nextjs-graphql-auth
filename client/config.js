27a58b0690b7b5692911adfc77fb91fd44338310
Nigel Reoch <nreoch9@gmail.com>
2019-01-12 09:21:13 -0500

    Added log

--- client/config.js
+++ client/config.js
@@ -1,3 +1,5 @@
 export const endpoint = process.browser
   ? "http://nextjsgraphqlauth-env.hgdpaicmfm.us-east-2.elasticbeanstalk.com/graphql"
   : "http://nginx/graphql";
+
+//? "http://nextjsgraphqlauth-env.hgdpaicmfm.us-east-2.elasticbeanstalk.com/graphql"
--- client/lib/withApollo.js
+++ client/lib/withApollo.js
@@ -3,6 +3,7 @@ import ApolloClient from "apollo-boost";
 import { endpoint } from "../config";
 
 function createClient({ headers }) {
+  console.log("NODE_ENV", process.env.NODE_ENV);
   return new ApolloClient({
     uri: endpoint,
     request: operation => {
